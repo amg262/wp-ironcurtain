25c98a3e5c41d6275a7c09e37f6f3ef026b9b142
Andrew Gunn <andrewmgunn26@gmail.com>
2018-09-12 03:18:21 -0500

    added all files in

--- oop-example.php
+++ oop-example.php
@@ -5,294 +5,298 @@
  *
  * @author Tareq Hasan
  */
-if ( !class_exists('WeDevs_Settings_API_Test' ) ):
-class WeDevs_Settings_API_Test {
+if ( ! class_exists( 'WeDevs_Settings_API_Test' ) ):
+	class WeDevs_Settings_API_Test {
 
-    private $settings_api;
+		private $settings_api;
 
-    function __construct() {
-        $this->settings_api = new WeDevs_Settings_API;
+		function __construct() {
+			$this->settings_api = new WeDevs_Settings_API;
 
-        add_action( 'admin_init', array($this, 'admin_init') );
-        add_action( 'admin_menu', array($this, 'admin_menu') );
-    }
+			add_action( 'admin_init', [ $this, 'admin_init' ] );
+			add_action( 'admin_menu', [ $this, 'admin_menu' ] );
+		}
 
-    function admin_init() {
+		function admin_init() {
 
-        //set the settings
-        $this->settings_api->set_sections( $this->get_settings_sections() );
-        $this->settings_api->set_fields( $this->get_settings_fields() );
+			//set the settings
+			$this->settings_api->set_sections( $this->get_settings_sections() );
+			$this->settings_api->set_fields( $this->get_settings_fields() );
 
-        //initialize settings
-        $this->settings_api->admin_init();
-    }
+			//initialize settings
+			$this->settings_api->admin_init();
+		}
 
-    function admin_menu() {
-        add_options_page( 'Settings API', 'Settings API', 'delete_posts', 'settings_api_test', array($this, 'plugin_page') );
-    }
+		function admin_menu() {
+			add_options_page( 'Settings API', 'Settings API', 'delete_posts', 'settings_api_test', [
+				$this,
+				'plugin_page',
+			] );
+		}
 
-    function get_settings_sections() {
-        $sections = array(
-            array(
-                'id' => 'wedevs_basics',
-                'title' => __( 'Basic Settings', 'wedevs' )
-            ),
-            array(
-                'id' => 'wedevs_advanced',
-                'title' => __( 'Advanced Settings', 'wedevs' )
-            ),
-            array(
-                'id' => 'wedevs_others',
-                'title' => __( 'Other Settings', 'wpuf' )
-            )
-        );
-        return $sections;
-    }
+		function get_settings_sections() {
+			$sections = [
+				[
+					'id'    => 'wedevs_basics',
+					'title' => __( 'Basic Settings', 'wedevs' ),
+				],
+				[
+					'id'    => 'wedevs_advanced',
+					'title' => __( 'Advanced Settings', 'wedevs' ),
+				],
+				[
+					'id'    => 'wedevs_others',
+					'title' => __( 'Other Settings', 'wpuf' ),
+				],
+			];
 
-    /**
-     * Returns all the settings fields
-     *
-     * @return array settings fields
-     */
-    function get_settings_fields() {
-        $settings_fields = array(
-            'wedevs_basics' => array(
-                array(
-                    'name'              => 'text_val',
-                    'label'             => __( 'Text Input', 'wedevs' ),
-                    'desc'              => __( 'Text input description', 'wedevs' ),
-                    'type'              => 'text',
-                    'default'           => 'Title',
-                    'sanitize_callback' => 'intval'
-                ),
-                array(
-                    'name'              => 'number_input',
-                    'label'             => __( 'Number Input', 'wedevs' ),
-                    'desc'              => __( 'Number field with validation callback `intval`', 'wedevs' ),
-                    'type'              => 'number',
-                    'default'           => 'Title',
-                    'sanitize_callback' => 'intval'
-                ),
-                array(
-                    'name'  => 'textarea',
-                    'label' => __( 'Textarea Input', 'wedevs' ),
-                    'desc'  => __( 'Textarea description', 'wedevs' ),
-                    'type'  => 'textarea'
-                ),
-                array(
-                    'name'  => 'checkbox',
-                    'label' => __( 'Checkbox', 'wedevs' ),
-                    'desc'  => __( 'Checkbox Label', 'wedevs' ),
-                    'type'  => 'checkbox'
-                ),
-                array(
-                    'name'    => 'radio',
-                    'label'   => __( 'Radio Button', 'wedevs' ),
-                    'desc'    => __( 'A radio button', 'wedevs' ),
-                    'type'    => 'radio',
-                    'options' => array(
-                        'yes' => 'Yes',
-                        'no'  => 'No'
-                    )
-                ),
-                array(
-                    'name'    => 'multicheck',
-                    'label'   => __( 'Multile checkbox', 'wedevs' ),
-                    'desc'    => __( 'Multi checkbox description', 'wedevs' ),
-                    'type'    => 'multicheck',
-                    'options' => array(
-                        'one'   => 'One',
-                        'two'   => 'Two',
-                        'three' => 'Three',
-                        'four'  => 'Four'
-                    )
-                ),
-                array(
-                    'name'    => 'selectbox',
-                    'label'   => __( 'A Dropdown', 'wedevs' ),
-                    'desc'    => __( 'Dropdown description', 'wedevs' ),
-                    'type'    => 'select',
-                    'default' => 'no',
-                    'options' => array(
-                        'yes' => 'Yes',
-                        'no'  => 'No'
-                    )
-                ),
-                array(
-                    'name'    => 'password',
-                    'label'   => __( 'Password', 'wedevs' ),
-                    'desc'    => __( 'Password description', 'wedevs' ),
-                    'type'    => 'password',
-                    'default' => ''
-                ),
-                array(
-                    'name'    => 'file',
-                    'label'   => __( 'File', 'wedevs' ),
-                    'desc'    => __( 'File description', 'wedevs' ),
-                    'type'    => 'file',
-                    'default' => '',
-                    'options' => array(
-                        'button_label' => 'Choose Image'
-                    )
-                )
-            ),
-            'wedevs_advanced' => array(
-                array(
-                    'name'    => 'color',
-                    'label'   => __( 'Color', 'wedevs' ),
-                    'desc'    => __( 'Color description', 'wedevs' ),
-                    'type'    => 'color',
-                    'default' => ''
-                ),
-                array(
-                    'name'    => 'password',
-                    'label'   => __( 'Password', 'wedevs' ),
-                    'desc'    => __( 'Password description', 'wedevs' ),
-                    'type'    => 'password',
-                    'default' => ''
-                ),
-                array(
-                    'name'    => 'wysiwyg',
-                    'label'   => __( 'Advanced Editor', 'wedevs' ),
-                    'desc'    => __( 'WP_Editor description', 'wedevs' ),
-                    'type'    => 'wysiwyg',
-                    'default' => ''
-                ),
-                array(
-                    'name'    => 'multicheck',
-                    'label'   => __( 'Multile checkbox', 'wedevs' ),
-                    'desc'    => __( 'Multi checkbox description', 'wedevs' ),
-                    'type'    => 'multicheck',
-                    'default' => array('one' => 'one', 'four' => 'four'),
-                    'options' => array(
-                        'one'   => 'One',
-                        'two'   => 'Two',
-                        'three' => 'Three',
-                        'four'  => 'Four'
-                    )
-                ),
-                array(
-                    'name'    => 'selectbox',
-                    'label'   => __( 'A Dropdown', 'wedevs' ),
-                    'desc'    => __( 'Dropdown description', 'wedevs' ),
-                    'type'    => 'select',
-                    'options' => array(
-                        'yes' => 'Yes',
-                        'no'  => 'No'
-                    )
-                ),
-                array(
-                    'name'    => 'password',
-                    'label'   => __( 'Password', 'wedevs' ),
-                    'desc'    => __( 'Password description', 'wedevs' ),
-                    'type'    => 'password',
-                    'default' => ''
-                ),
-                array(
-                    'name'    => 'file',
-                    'label'   => __( 'File', 'wedevs' ),
-                    'desc'    => __( 'File description', 'wedevs' ),
-                    'type'    => 'file',
-                    'default' => ''
-                )
-            ),
-            'wedevs_others' => array(
-                array(
-                    'name'    => 'text',
-                    'label'   => __( 'Text Input', 'wedevs' ),
-                    'desc'    => __( 'Text input description', 'wedevs' ),
-                    'type'    => 'text',
-                    'default' => 'Title'
-                ),
-                array(
-                    'name'  => 'textarea',
-                    'label' => __( 'Textarea Input', 'wedevs' ),
-                    'desc'  => __( 'Textarea description', 'wedevs' ),
-                    'type'  => 'textarea'
-                ),
-                array(
-                    'name'  => 'checkbox',
-                    'label' => __( 'Checkbox', 'wedevs' ),
-                    'desc'  => __( 'Checkbox Label', 'wedevs' ),
-                    'type'  => 'checkbox'
-                ),
-                array(
-                    'name'    => 'radio',
-                    'label'   => __( 'Radio Button', 'wedevs' ),
-                    'desc'    => __( 'A radio button', 'wedevs' ),
-                    'type'    => 'radio',
-                    'options' => array(
-                        'yes' => 'Yes',
-                        'no'  => 'No'
-                    )
-                ),
-                array(
-                    'name'    => 'multicheck',
-                    'label'   => __( 'Multile checkbox', 'wedevs' ),
-                    'desc'    => __( 'Multi checkbox description', 'wedevs' ),
-                    'type'    => 'multicheck',
-                    'options' => array(
-                        'one'   => 'One',
-                        'two'   => 'Two',
-                        'three' => 'Three',
-                        'four'  => 'Four'
-                    )
-                ),
-                array(
-                    'name'    => 'selectbox',
-                    'label'   => __( 'A Dropdown', 'wedevs' ),
-                    'desc'    => __( 'Dropdown description', 'wedevs' ),
-                    'type'    => 'select',
-                    'options' => array(
-                        'yes' => 'Yes',
-                        'no'  => 'No'
-                    )
-                ),
-                array(
-                    'name'    => 'password',
-                    'label'   => __( 'Password', 'wedevs' ),
-                    'desc'    => __( 'Password description', 'wedevs' ),
-                    'type'    => 'password',
-                    'default' => ''
-                ),
-                array(
-                    'name'    => 'file',
-                    'label'   => __( 'File', 'wedevs' ),
-                    'desc'    => __( 'File description', 'wedevs' ),
-                    'type'    => 'file',
-                    'default' => ''
-                )
-            )
-        );
+			return $sections;
+		}
 
-        return $settings_fields;
-    }
+		/**
+		 * Returns all the settings fields
+		 *
+		 * @return array settings fields
+		 */
+		function get_settings_fields() {
+			$settings_fields = [
+				'wedevs_basics'   => [
+					[
+						'name'              => 'text_val',
+						'label'             => __( 'Text Input', 'wedevs' ),
+						'desc'              => __( 'Text input description', 'wedevs' ),
+						'type'              => 'text',
+						'default'           => 'Title',
+						'sanitize_callback' => 'intval',
+					],
+					[
+						'name'              => 'number_input',
+						'label'             => __( 'Number Input', 'wedevs' ),
+						'desc'              => __( 'Number field with validation callback `intval`', 'wedevs' ),
+						'type'              => 'number',
+						'default'           => 'Title',
+						'sanitize_callback' => 'intval',
+					],
+					[
+						'name'  => 'textarea',
+						'label' => __( 'Textarea Input', 'wedevs' ),
+						'desc'  => __( 'Textarea description', 'wedevs' ),
+						'type'  => 'textarea',
+					],
+					[
+						'name'  => 'checkbox',
+						'label' => __( 'Checkbox', 'wedevs' ),
+						'desc'  => __( 'Checkbox Label', 'wedevs' ),
+						'type'  => 'checkbox',
+					],
+					[
+						'name'    => 'radio',
+						'label'   => __( 'Radio Button', 'wedevs' ),
+						'desc'    => __( 'A radio button', 'wedevs' ),
+						'type'    => 'radio',
+						'options' => [
+							'yes' => 'Yes',
+							'no'  => 'No',
+						],
+					],
+					[
+						'name'    => 'multicheck',
+						'label'   => __( 'Multile checkbox', 'wedevs' ),
+						'desc'    => __( 'Multi checkbox description', 'wedevs' ),
+						'type'    => 'multicheck',
+						'options' => [
+							'one'   => 'One',
+							'two'   => 'Two',
+							'three' => 'Three',
+							'four'  => 'Four',
+						],
+					],
+					[
+						'name'    => 'selectbox',
+						'label'   => __( 'A Dropdown', 'wedevs' ),
+						'desc'    => __( 'Dropdown description', 'wedevs' ),
+						'type'    => 'select',
+						'default' => 'no',
+						'options' => [
+							'yes' => 'Yes',
+							'no'  => 'No',
+						],
+					],
+					[
+						'name'    => 'password',
+						'label'   => __( 'Password', 'wedevs' ),
+						'desc'    => __( 'Password description', 'wedevs' ),
+						'type'    => 'password',
+						'default' => '',
+					],
+					[
+						'name'    => 'file',
+						'label'   => __( 'File', 'wedevs' ),
+						'desc'    => __( 'File description', 'wedevs' ),
+						'type'    => 'file',
+						'default' => '',
+						'options' => [
+							'button_label' => 'Choose Image',
+						],
+					],
+				],
+				'wedevs_advanced' => [
+					[
+						'name'    => 'color',
+						'label'   => __( 'Color', 'wedevs' ),
+						'desc'    => __( 'Color description', 'wedevs' ),
+						'type'    => 'color',
+						'default' => '',
+					],
+					[
+						'name'    => 'password',
+						'label'   => __( 'Password', 'wedevs' ),
+						'desc'    => __( 'Password description', 'wedevs' ),
+						'type'    => 'password',
+						'default' => '',
+					],
+					[
+						'name'    => 'wysiwyg',
+						'label'   => __( 'Advanced Editor', 'wedevs' ),
+						'desc'    => __( 'WP_Editor description', 'wedevs' ),
+						'type'    => 'wysiwyg',
+						'default' => '',
+					],
+					[
+						'name'    => 'multicheck',
+						'label'   => __( 'Multile checkbox', 'wedevs' ),
+						'desc'    => __( 'Multi checkbox description', 'wedevs' ),
+						'type'    => 'multicheck',
+						'default' => [ 'one' => 'one', 'four' => 'four' ],
+						'options' => [
+							'one'   => 'One',
+							'two'   => 'Two',
+							'three' => 'Three',
+							'four'  => 'Four',
+						],
+					],
+					[
+						'name'    => 'selectbox',
+						'label'   => __( 'A Dropdown', 'wedevs' ),
+						'desc'    => __( 'Dropdown description', 'wedevs' ),
+						'type'    => 'select',
+						'options' => [
+							'yes' => 'Yes',
+							'no'  => 'No',
+						],
+					],
+					[
+						'name'    => 'password',
+						'label'   => __( 'Password', 'wedevs' ),
+						'desc'    => __( 'Password description', 'wedevs' ),
+						'type'    => 'password',
+						'default' => '',
+					],
+					[
+						'name'    => 'file',
+						'label'   => __( 'File', 'wedevs' ),
+						'desc'    => __( 'File description', 'wedevs' ),
+						'type'    => 'file',
+						'default' => '',
+					],
+				],
+				'wedevs_others'   => [
+					[
+						'name'    => 'text',
+						'label'   => __( 'Text Input', 'wedevs' ),
+						'desc'    => __( 'Text input description', 'wedevs' ),
+						'type'    => 'text',
+						'default' => 'Title',
+					],
+					[
+						'name'  => 'textarea',
+						'label' => __( 'Textarea Input', 'wedevs' ),
+						'desc'  => __( 'Textarea description', 'wedevs' ),
+						'type'  => 'textarea',
+					],
+					[
+						'name'  => 'checkbox',
+						'label' => __( 'Checkbox', 'wedevs' ),
+						'desc'  => __( 'Checkbox Label', 'wedevs' ),
+						'type'  => 'checkbox',
+					],
+					[
+						'name'    => 'radio',
+						'label'   => __( 'Radio Button', 'wedevs' ),
+						'desc'    => __( 'A radio button', 'wedevs' ),
+						'type'    => 'radio',
+						'options' => [
+							'yes' => 'Yes',
+							'no'  => 'No',
+						],
+					],
+					[
+						'name'    => 'multicheck',
+						'label'   => __( 'Multile checkbox', 'wedevs' ),
+						'desc'    => __( 'Multi checkbox description', 'wedevs' ),
+						'type'    => 'multicheck',
+						'options' => [
+							'one'   => 'One',
+							'two'   => 'Two',
+							'three' => 'Three',
+							'four'  => 'Four',
+						],
+					],
+					[
+						'name'    => 'selectbox',
+						'label'   => __( 'A Dropdown', 'wedevs' ),
+						'desc'    => __( 'Dropdown description', 'wedevs' ),
+						'type'    => 'select',
+						'options' => [
+							'yes' => 'Yes',
+							'no'  => 'No',
+						],
+					],
+					[
+						'name'    => 'password',
+						'label'   => __( 'Password', 'wedevs' ),
+						'desc'    => __( 'Password description', 'wedevs' ),
+						'type'    => 'password',
+						'default' => '',
+					],
+					[
+						'name'    => 'file',
+						'label'   => __( 'File', 'wedevs' ),
+						'desc'    => __( 'File description', 'wedevs' ),
+						'type'    => 'file',
+						'default' => '',
+					],
+				],
+			];
 
-    function plugin_page() {
-        echo '<div class="wrap">';
+			return $settings_fields;
+		}
 
-        $this->settings_api->show_navigation();
-        $this->settings_api->show_forms();
+		function plugin_page() {
+			echo '<div class="wrap">';
 
-        echo '</div>';
-    }
+			$this->settings_api->show_navigation();
+			$this->settings_api->show_forms();
 
-    /**
-     * Get all the pages
-     *
-     * @return array page names with key value pairs
-     */
-    function get_pages() {
-        $pages = get_pages();
-        $pages_options = array();
-        if ( $pages ) {
-            foreach ($pages as $page) {
-                $pages_options[$page->ID] = $page->post_title;
-            }
-        }
+			echo '</div>';
+		}
 
-        return $pages_options;
-    }
+		/**
+		 * Get all the pages
+		 *
+		 * @return array page names with key value pairs
+		 */
+		function get_pages() {
+			$pages         = get_pages();
+			$pages_options = [];
+			if ( $pages ) {
+				foreach ( $pages as $page ) {
+					$pages_options[ $page->ID ] = $page->post_title;
+				}
+			}
 
-}
+			return $pages_options;
+		}
+
+	}
 endif;
